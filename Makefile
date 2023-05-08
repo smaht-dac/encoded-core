3b385bcaa3cf7594c47f87036e6a2118c1db0571
William Ronchetti <wrr33@cornell.edu>
2023-05-08 14:01:17 -0400

    fix build

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ test:
 	make test-any
 
 remote-test:
-	make test-any
+	poetry run python -m pytest -xvv -r w --timeout=200 --es search-fourfront-testing-opensearch-kqm7pliix4wgiu4druk2indorq.us-east-1.es.amazonaws.com:443
 
 info:
 	@: $(info Here are some 'make' options:)
--- conftest.py
+++ conftest.py
@@ -1,9 +1,27 @@
+import pytest
 import tempfile
 
 
 pytest_plugins = [
     'src.encoded_core.tests.datafixtures'
 ]
+
+
+def pytest_addoption(parser):
+    parser.addoption("--es", action="store", default="", dest='es',
+                     help="use a remote es for testing")
+    parser.addoption("--aws-auth", action="store_true",
+                     help="connect using aws authorization")
+
+
+@pytest.fixture(scope='session')
+def remote_es(request):
+    return request.config.getoption("--es")
+
+
+@pytest.fixture(scope='session')
+def aws_auth(request):
+    return request.config.getoption("--aws-auth")
 
 
 def pytest_configure():
