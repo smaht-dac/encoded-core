190be065161aab946f9897057a187bc514f407d9
William Ronchetti <wrr33@cornell.edu>
2023-08-07 09:57:47 -0400

    new utils, updated defs

--- src/encoded_core/project_defs.py
+++ src/encoded_core/project_defs.py
@@ -1,10 +1,8 @@
 from dcicutils.project_utils import C4ProjectRegistry
 from snovault.project_defs import SnovaultProject
-from .project_env import APPLICATION_NAME, APPLICATION_PYPROJECT_NAME
 
 
-@C4ProjectRegistry.register(APPLICATION_PYPROJECT_NAME)
+@C4ProjectRegistry.register('encoded-core')
 class EncodedCoreProject(SnovaultProject):
-    NAMES = {'NAME': APPLICATION_NAME, 'PYPI_NAME': APPLICATION_PYPROJECT_NAME}
+    NAMES = {'NAME': 'core', 'PYPI_NAME': 'encoded-core'}
     ACCESSION_PREFIX = 'ENC'
-    PROJECT_NAME = 'encoded_core'
